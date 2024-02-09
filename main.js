dd1158628d5e1ef748e706aa364abcdc2f5a08f5
Ton <146097352+wiangin@users.noreply.github.com>
2024-02-09 13:44:03 +0100

    small change

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import Pikachu from "./modules/Pikachu.js";
+import Sanchai from "./modules/Sanchai.js";
 
 const pikachu = new Pikachu()
 console.log(pikachu);
+
+const sanchai = new Sanchai();
+console.log(sanchai);
